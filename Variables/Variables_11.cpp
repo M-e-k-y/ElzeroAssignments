4201dfa6a3bdc38c9f5d1e8721323d04379bf980
Mekawy <ahmedmagdy3fg@gmail.com>
2024-03-07 12:12:08 -0800

    Add Variables

---
+++ Variables/Variables_11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello Elzero";
+  cout << " Web School";
+  cout << " I Love Programming";
+  cout << " Too Much";
+  cout << " Specially C++\n";
+  return 0;
+}
+
+// Needed Output
+//"Hello Elzero Web School I Love Programming Too Much Specially C++"
---
+++ Variables/Variables_3.cpp
@@ -0,0 +1,16 @@
+/*
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ not valid ] => bad practice ?
+  __name      [ valid ] => good practice ?
+  name@name   [ not valid ] => bad practice ?
+  name10name  [ valid ] => good practice ?
+  name!name   [ not valid ] => bad practice ?
+  first_NAME  [ valid ] => good practice ?
+  first_name  [ valid ] => good practice ?
+  firstName   [ valid ] => good practice ?
+  first name  [ not valid ] => bad practice ?
+  fn          [ valid ] => good practice ?
+  public      [ not valid ] => bad practice ?
+  Public      [ not valid ] => bad practice ?
+*/
