9f220d6316ed54984719e7d3f541fe2b7593841a
Mekawy <ahmedmagdy3fg@gmail.com>
2024-03-07 12:24:49 -0800

    Add Variables

---
+++ Variables/Variables_9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+cout << " enter the num \n ---------------\n" ;
+int num  ;
+cin >> num ;
+cout << "result is : " <<num * num ;
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
